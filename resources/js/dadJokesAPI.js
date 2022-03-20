055a3dad626bef505a342d1106b46d83e31068de
shaddyjr <mahdis.pw@gmail.com>
2022-03-20 06:21:57 -0400

    simplify getJokesPage function

--- resources/js/dadJokesAPI.js
+++ resources/js/dadJokesAPI.js
@@ -1,10 +1,14 @@
 // Check out https://icanhazdadjoke.com/api
 const getJokesByPage = async (page = 1) => {
 	const URL = `https://icanhazdadjoke.com/search?limit=30&page=${page}`;
-	const [results, previous_page] = await fetch(URL, {
+	const json = await fetch(URL, {
 		headers: { Accept: "application/json" },
-	})
-		.then((response) => response.json())
-		.then(({ results, previous_page }) => [results, previous_page]);
+	}).then((response) => response.json());
+
+	// TODO: REMOVE AFTER TESTING FINISHED
+	console.log({ json });
+	return { json };
+
+	const { results, previous_page } = json;
 	return [results, previous_page];
 };
